669111890667617ccb79847ae7bc7105cb5d5be5
minal <minalnarsale@gmail.com>
2019-01-23 11:26:14 +0100

    DB connection fix with getUserById API

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,30 @@
 'use strict'
 
+const mysql = require('mysql')
+const {host,user,password,database} = require('./../config')
+
+//creating database connection
+var con = mysql.createConnection({
+    host: host,
+    user: user,
+    password: password,
+    database: database
+});
+con.connect(function (err) {
+    if(err)
+        console.log("database connection failed");
+    else
+        console.log("database connection successful!");
+});
+
 exports.getUserById = function(req, res){
+
     console.log("req : " +req.url.substring(req.url.lastIndexOf("/") + 1, req.url.length));
     var userId = req.url.substring(req.url.lastIndexOf("/") + 1, req.url.length);
-    res.send("userId : " + userId);
+
+    con.query('SELECT*FROM user where userId=' + userId, function (err, result) {
+        res.status(200);
+        res.setHeader('Content-Type', 'application/json');
+        res.send({"data":result});
+    });
 }
--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-const mysql = require('mysql')
-const {host,user,password,database} = require('./config')
 const express = require('express')
 
 var app = express();
 var serverPort = 8080;
-
-//connecting to database
-var con = mysql.createConnection({
-    host: host,
-    user: user,
-    password: password,
-    database: database
-});
-con.connect(function (err) {
-    if(err)
-        console.log("database connection failed");
-    else
-        console.log("database connection successful!");
-});
 
 //defining routes
 app.use('/', require('./routes'));
